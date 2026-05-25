cc7b8b2b151d2b897db7725c30233061c86231e5
sqveren <sqveren@gmail.com>
2026-05-25 11:33:42 +0300

    Initialize project structure and setup webdriver fixture

---
+++ conftest.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+import pytest
+
+@pytest.fixture()
+def driver():
+
+    service = Service(
+        executable_path=ChromeDriverManager().install()
+    )
+
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("")
+
+    driver = webdriver.Chrome(
+        service=service,
+        options=options
+    )
+
+    yield driver
+
+    driver.quit()
