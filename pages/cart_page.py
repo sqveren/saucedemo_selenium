e79bb672b98fdd8dc2324c18accee3b4037d2fc1
sqveren <sqveren@gmail.com>
2026-05-27 12:37:17 +0300

    Cart page methods

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 
@@ -12,10 +13,25 @@ class CartPage(BasePage):
     CHECKOUT_BUTTON = (By.ID, "checkout")
 
     def get_cart_items_count(self):
+        elements = self.driver.find_elements(*self.CART_ITEM)
+        return len(elements)
 
     def remove_all_from_cart(self):
+            buttons = self.driver.find_elements(*self.REMOVE_BUTTONS)
+            for button in buttons:
+                button.click()
+
 
     def go_to_checkout(self):
+        self.wait.until(
+            EC.element_to_be_clickable(self.CHECKOUT_BUTTON)
+        ).click()
 
-    def back_to_checkout(self):
 
+    def back_to_shopping(self):
+        self.wait.until(
+            EC.element_to_be_clickable(self.CONTINUE_SHOPPING_BUTTON)
+        ).click()
+
+    def is_cart_empty(self):
+        return len(self.driver.find_elements(*self.CART_ITEM)) == 0
